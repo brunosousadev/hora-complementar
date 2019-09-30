f79407a453ecf46fceda16f497e0a6121143c608
Bruno Sousa <brse01@gmail.com>
2019-09-30 10:21:30 -0300

    #7 standardizing the routes

--- backend/start/routes.js
+++ backend/start/routes.js
@@ -4,28 +4,33 @@
 const Route = use('Route')
 
 
-Route.post('/sessions','SessionController.store').validator('Session');
-Route.post('/forgotPassword', 'ForgotPasswordController.store').validator('Forgot');
-Route.post('/reset', 'ResetPasswordController.store').validator('Reset');
+Route.post('sessions','SessionController.store').validator('Session');
+Route.post('forgotPassword', 'ForgotPasswordController.store').validator('Forgot');
+Route.post('reset', 'ResetPasswordController.store').validator('Reset');
 
 
-Route.get('/courses', 'CourseController.index');
-Route.get('/courses/:id', 'CourseController.show');
-Route.post('/courses', 'CourseController.store').validator('Course');
-Route.put('/courses/:id', 'CourseController.update').validator('Course');
-Route.delete('/courses/:id', 'CourseController.destroy');
+Route.get('courses', 'CourseController.index');
+Route.get('courses/:id', 'CourseController.show');
+Route.post('courses', 'CourseController.store').validator('Course');
+Route.put('courses/:id', 'CourseController.update').validator('Course');
+Route.delete('courses/:id', 'CourseController.destroy');
 
-Route.get('/files/:file','FileController.show');
-Route.post('/users', 'UserController.store').validator('User');
+Route.get('files/:file','FileController.show');
+Route.post('users', 'UserController.store').validator('User');
 
 Route.group(() => {
-    Route.get('/users', 'UserController.index');
+
+    Route.get('users', 'UserController.index');
     Route.get('users/:id','UserController.show');    
-    Route.put('/profile','ProfileController.update').validator('Profile')
-    Route.delete('/users/:id', 'UserController.destroy');
+    Route.put('profile','ProfileController.update').validator('Profile')
+    Route.delete('users/:id', 'UserController.destroy');
 
 
-    Route.post('/categories', 'CategoryController.store').validator('Category');
+    Route.post('categories', 'CategoryController.store').validator('Category');
+    Route.get('categories', 'CategoryController.index');
+
+    Route.get('categories/:id','CategoryController.show');    
+    Route.put('categories/:id','CategoryController.update');
 
 }).middleware(['auth']);
 
